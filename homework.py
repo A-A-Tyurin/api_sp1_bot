d2a495b1560336003d14291a681a55e0deb62521
A-A-Tyurin <avt2@yandex.ru>
2021-06-24 17:35:46 +0300

    review fix

--- homework.py
+++ homework.py
@@ -20,9 +20,16 @@ logger.addHandler(handler)
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-HOMEWORK_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+HOMEWORK_API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+
+HOMEWORK_STATUSES = {
+    'reviewing': 'Работа взята в ревью.',
+    'rejected': 'К сожалению, в работе нашлись ошибки.',
+    'approved': 'Ревьюеру всё понравилось, работа зачтена!'
+}
 
 bot = telegram.Bot(TELEGRAM_TOKEN)
 
@@ -31,37 +38,39 @@ def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
 
-    if homework_status == 'reviewing':
-        verdict = 'Работа взята в ревью.'
-    if homework_status == 'rejected':
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+    if homework_name is None and homework_status is None:
+        raise ValueError('Аргумент homework не правильной структуры.')
 
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    verdict = HOMEWORK_STATUSES[homework_status]
+
+    return f'Проект "{homework_name}"!\n\n{verdict}'
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = requests.get(
-        HOMEWORK_API,
-        params={'from_date': current_timestamp},
-        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    )
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            HOMEWORK_API,
+            params={'from_date': current_timestamp},
+            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+        )
+        return homework_statuses.json()
+    except Exception:
+        logger.exception()
+        raise
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, text=message)
+    return bot.send_message(TELEGRAM_CHAT_ID, text=message)
 
 
 def main():
     current_timestamp = int(time.time())
-    sleep_time = 5 * 60
 
     while True:
         try:
             homeworks = get_homeworks(current_timestamp)
             homeworks_list = homeworks.get('homeworks')
+            current_timestamp = homeworks.get('current_date')
 
             for homework in homeworks_list:
                 message = parse_homework_status(homework)
@@ -71,9 +80,9 @@ def main():
             send_message(f'Бот упал с ошибкой: {e}')
             logger.exception()
         finally:
-            time.sleep(sleep_time)
-            current_timestamp += sleep_time
+            time.sleep(5 * 60)
 
 
 if __name__ == '__main__':
+    logger.debug('Бот запустился')
     main()
